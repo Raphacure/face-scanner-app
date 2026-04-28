b08e0d027a78d6def6aaa5f8a1fbd7340077244b
Dileep-Mallena <mallenadileep90@gmail.com>
2026-04-28 13:18:56 +0530

    Restructure API route prefixes for consistency.

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,7 +4,7 @@ from app.routes.db_routes import router as db_router
 from app.routes.scan_routes import router as scan_router
 from app.routes.user_routes import router as user_router
 
-router = APIRouter()
+router = APIRouter(prefix="/api/v1")
 router.include_router(scan_router)
 router.include_router(db_router)
 router.include_router(user_router)
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.controllers.user_controller import get_user_by_id_controller
 
-router = APIRouter(prefix="/users", tags=["users"])
+router = APIRouter(prefix="/user", tags=["user"])
 
 
 @router.get("/{user_id}")
